7a5ec45e966f8523d5c21c19310712176a9121cf
Karim Mohamed <komicho1996@gmail.com>
2023-05-25 11:16:05 +0300

    Feat: use the registrar to register this module

--- registration.php
+++ registration.php
@@ -1,42 +1,43 @@
 <?php
 
-use StorePHP\StoreWays\PluginRegistrar;
+use StorePHP\StoreWays\Registrar;
 use Storeways\Shipper\Http\Livewire\Countries\CountryCreate;
 
-// initModule
-PluginRegistrar::initModule('storeways_shipper', [
-    'icon' => 'cube-send',
-    'name' => 'Shipper',
-    'slug' => 'shipper',
-    'order' => 500,
-], function ($links) {
-    return [
-        $links->addLink(
-            icon:'world',
-            name:'Countries',
-            route:'#',
-            order:10,
-        ),
-        $links->addLink(
-            icon:'map-2',
-            name:'Cities',
-            route:'#',
-            order:20,
-        ),
-        $links->addLink(
-            icon:'cash',
-            name:'Costs',
-            route:'#',
-            order:30,
-        ),
-    ];
-});
-
-// loadMigrations
-PluginRegistrar::loadMigrations(__DIR__ . '/database/migrations');
-
-// loadRoutes
-\StorePHP\StoreWays\PluginRegistrar::loadRoutes(__DIR__ . '/routes/web.php', 'shipper');
-
-// loadLivewireComponent
-\StorePHP\StoreWays\PluginRegistrar::loadLivewireComponent('storeways-shipper-country-create', CountryCreate::class);
+Registrar::module(
+    id:'storewys_shipper',
+    info:[
+        'name' => 'Shipper',
+    ],
+    sidebar:[
+        'icon' => 'cube-send',
+        'name' => 'Shipper',
+    ],
+    menu:function ($links) {
+        return [
+            $links->addLink(
+                icon:'world',
+                name:'Countries',
+                route:'dashboard',
+                order:10,
+            ),
+            $links->addLink(
+                icon:'map-2',
+                name:'Cities',
+                route:'dashboard',
+                order:20,
+            ),
+            $links->addLink(
+                icon:'cash',
+                name:'Costs',
+                route:'dashboard',
+                order:30,
+            ),
+        ];
+    },
+    migrations:__DIR__ . '/database/migrations',
+    routes:['shipper', __DIR__ . '/routes/web.php'],
+    // views:['shipper', __DIR__ . '/views'],
+    livewireComponents:[
+        'storeways-shipper-country-create' => CountryCreate::class,
+    ]
+);
